f89c38d4bef7b98d13d344fc887ca933ba853894
phamquyetthang <quyetthang,phucchi@gmail.com>
2023-02-23 00:06:20 +0700

    API get total item in user

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,17 +1,28 @@
 import { SentryInterceptor } from './../sentry.interceptor';
-import { Controller, Get, Post, Body, Patch, Param, Delete, UseInterceptors } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  UseInterceptors,
+  UseGuards,
+  Request,
+} from '@nestjs/common';
 import { UserService } from './user.service';
 import { UpdateUserDto } from './dto/update-user.dto';
-
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 
 @UseInterceptors(SentryInterceptor)
-@Controller('user')
+@Controller('/api/v1/user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
+  @UseGuards(JwtAuthGuard)
   @Get()
-  findAll() {
-    return this.userService.findAll();
+  getMyInfo(@Request() req) {
+    return this.userService.getMyInfo(req.user);
   }
 
   @Get(':id')
