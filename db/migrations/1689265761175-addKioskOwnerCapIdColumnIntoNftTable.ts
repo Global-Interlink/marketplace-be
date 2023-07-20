e2e5a85b04ccf9520dc8d2c365b8f31c9452748e
khanhv <nquockhanh0209v@gmail.com>
2023-07-20 10:03:44 +0700

    migrate db where typeModule IS NULL

--- db/migrations/1689265761175-addKioskOwnerCapIdColumnIntoNftTable.ts
+++ db/migrations/1689265761175-addKioskOwnerCapIdColumnIntoNftTable.ts
@@ -5,7 +5,7 @@ export class addKioskOwnerCapIdColumnIntoNftTable1689265761175 implements Migrat
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`ALTER TABLE "event_log" ADD "typeModule" character varying`);
-        await queryRunner.query(`UPDATE event_log SET typeModule = "marketplace" WHERE typeModule = null`);
+        await queryRunner.query(`UPDATE event_log SET typeModule = "marketplace" WHERE typeModule IS NULL`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
