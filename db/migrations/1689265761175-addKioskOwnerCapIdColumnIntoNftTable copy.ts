0ee1160601a06c1e0e26a159c6d86a3f8c43d024
khanhv <nquockhanh0209v@gmail.com>
2023-07-20 16:30:28 +0700

    add to 1 migrate file

--- db/migrations/1689265761175-addKioskOwnerCapIdColumnIntoNftTable copy.ts
+++ db/migrations/1689265761175-addKioskOwnerCapIdColumnIntoNftTable copy.ts
@@ -4,8 +4,7 @@ export class addKioskOwnerCapIdColumnIntoNftTable1689265761175 implements Migrat
     name = 'addKioskOwnerCapIdColumnIntoNftTable1689265761175'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "event_log" ADD "typeModule" character varying`);
-        
+        await queryRunner.query(`ALTER TABLE "event_log" ADD "typeModule" character varying DEFAULT 'marketplace'`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
